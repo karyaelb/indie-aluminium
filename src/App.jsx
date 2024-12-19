3cb30f4ad9ee5e759742e0754d9e1f703151bfa0
Karya Lingga <usscharlesberry@gmail.com>
2024-12-20 04:54:45 +0700

    metadata again

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,10 @@ import Home from "./components/Home";
 import Catalog from "./components/Catalog";
 import Gallery from "./components/Gallery";
 import Footer from "./components/Footer";
-import Metadata from "./Metadata";
 
 const App = () => {
   return (
     <>
-      <Metadata
-        title="Indie Aluminium"
-        description="Kami melayani pemasangan aluminium seperti pintu, kusen,
-        jendela dan partisi lain di Sidoarjo dan sekitarnya."
-        icon="/logo.jpg"
-        openGraph={{
-          images: ["logo.jpg"],
-        }}
-      />
       <Router>
         <div>
           {/* Routing */}
